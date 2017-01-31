b5f1475a91a0620ff63cc0fe005b4d6565f34c3c
Yuan <yuanbinhang@hotmail.com>
2017-01-31 14:04:15 -0600

    update one page

---
+++ userStudyPage/question.js
@@ -0,0 +1,136 @@
+var editor;
+var questions;
+var index = 0;
+var secondCount = 0;
+
+const alert=(message)=>{
+    let div = document.getElementById("alertMessage");
+    div.innerHTML = message;
+    div.setAttribute("class","alert alert-danger");
+}
+
+const clearAlert=()=>{
+    let div = document.getElementById("alertMessage");
+    div.innerHTML = "";
+    div.setAttribute("class","");
+}
+
+
+const timeFunction = ()=>{
+    let mins = Math.floor(secondCount/60);
+    let seconds = secondCount%60;
+    let string = "";
+    if(mins>=60){
+        throw "Too long for this task! Invalid case!"
+    }
+    if(mins<10){
+        string += '0';
+    }    
+    string += mins;
+    string+=':';
+    if(seconds<10){
+        string+='0';
+    }
+    string+=seconds;
+    document.getElementById("timer").innerHTML = string;
+    secondCount += 1;
+}
+
+var timer = setInterval(timeFunction,1000);
+
+const setQuestion = (i)=>{
+    console.log(i)
+    editor.setValue(questions[i].code);
+    let alternatives = document.getElementById("alternatives");
+    while (alternatives.firstChild){
+        alternatives.removeChild(alternatives.firstChild);
+    }
+    questions[i].Alternatives.forEach((item,i)=>{
+        console.log(item);
+        let option = document.createElement("div");
+        option.setAttribute("class","radio");
+        let label = document.createElement("label");
+        let input = document.createElement("input");
+        input.setAttribute("type","radio");
+        input.setAttribute("name","optradio");
+        input.setAttribute("id","opt"+i);
+        label.appendChild(input);
+        label.appendChild(document.createTextNode(item));
+        option.appendChild(label);
+        alternatives.appendChild(option);
+    })
+}
+
+const nextClickHandler = ()=>{
+    console.log("Next click Handler")
+    if(index<questions.length){
+        let current = document.getElementById("nav"+(index));
+        current.setAttribute("class","");
+        //Stub record the solution!
+        let selected = -1;
+        questions[index].Alternatives.forEach((item,i)=>{
+            let input = document.getElementById("opt"+i);
+            if(input.checked){
+                if(selected!==-1){
+                    throw "Error in radio check!";
+                }
+                selected = i;
+            }
+        })
+        if(selected === -1){
+            alert("Please finish this question first!");
+        }
+        else{
+            questions[index]['userSelected'] = selected;
+            questions[index]['timeCount'] = secondCount;
+            clearAlert();
+            //Update timer:
+            document.getElementById("timer").innerHTML = "00:00";
+            secondCount = 0;
+            index += 1;
+            if(index<questions.length){
+                setQuestion(index);
+                let next = document.getElementById("nav"+(index));
+                next.setAttribute("class","active");
+            }
+            else{
+                console.log(questions);
+                window.location.href = "thankyou.html"
+            }
+        }
+    }
+    else{
+        throw "Invalid index in nextClickHandler!";
+    }
+}
+
+
+window.onload = function(){
+	editor = ace.edit("editor");
+    editor.setTheme("ace/theme/chrome");
+    editor.getSession().setMode("ace/mode/java");
+
+    d3.json("./questionStub.json",function(error,json){
+        if (error) throw error;
+        questions = json.questions;
+        console.log(questions);
+        let nextButton = document.getElementById("nextButton");
+        nextButton.addEventListener("click",nextClickHandler);
+        setQuestion(index);
+        //index += 1;
+        let nav = document.getElementById("questionNavList");
+        questions.forEach((item,i)=>{
+            let li = document.createElement("li");
+            let a = document.createElement("a");
+            a.appendChild(document.createTextNode("Question "+(i+1)));
+            a.setAttribute("href","#");
+            li.appendChild(a);
+            if(i==0){
+                li.setAttribute("class","active");
+            }
+            li.setAttribute("id","nav"+i);
+            nav.appendChild(li);
+        })
+
+    })
+}
