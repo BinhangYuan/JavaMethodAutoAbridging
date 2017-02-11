2b1e2955069ad7b3bc69a1a7e9987ab7bad6bd10
BinhangYuan <yuanbinhang@hotmail.com>
2017-02-10 23:21:02 -0600

    2 types of survey question is done!

--- userStudy/FrontEnd/src/components/question/descriptionConst.js
+++ userStudy/FrontEnd/src/components/question/descriptionConst.js
@@ -1,4 +1,4 @@
-export const T1description = "";
+export const T1description = "Try determine the correct code quickly as possible, while also ensuring that you have determined the correct code with \'reasonable\' accurately, however you choose to define \'reasonable\'.  If at some point you determine that it is impossible to locate the correct code, then simply choose the code that is your best guess as to the answer.";
 export const T2description = "Try to complete each matching as quickly as possible, while also ensuring that you have labeled each code with \'reasonable\' accurately, however you choose to define \'reasonable\'.  If you determine that it is impossible to decide with more than 50/50 (coin-flip) accuracy which documentation goes with which method, then choose \'impossible to decide\'.";
 export const T3description = "Try to write a one-sentence description of the method. This description should inform someone at a high level what the method does. Try to write your description quickly as possible, while also ensuring that your description is of \'reasonable\' quality, however you define \'reasonable\'.";
 
@@ -21,4 +21,3 @@ export const surveyConfig = [
 		options: ["agree", "neither agree nor disagree", "disagree"]
 	},
 ]
-
